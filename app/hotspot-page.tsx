99a0497b90def669e7fe73e7aa3e88359d558a2b
FabLeo451 <fab.leo@gmail.com>
2025-12-09 23:17:33 +0100

    Created hotspot details page

--- app/hotspot-page.tsx
+++ app/hotspot-page.tsx
@@ -1,38 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import {
-	View,
-	TouchableOpacity,
-	Modal,
-	StyleSheet,
 	Alert,
+	View,
+	TextInput,
+	TouchableOpacity,
 	Platform,
-	Linking
+	Switch
 } from 'react-native';
-import { Text, Button } from '@ui-kitten/components';
-import { Ionicons } from '@expo/vector-icons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { router, useLocalSearchParams } from 'expo-router';
+import { router } from 'expo-router';
+import { Layout, Text, TextProps, Input, Button, Spinner } from '@ui-kitten/components';
 import { styles } from "@/Style";
-import { Hotspot } from '@/lib/hotspot'
+import { Ionicons } from '@expo/vector-icons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useLocalSearchParams } from 'expo-router';
+import { Hotspot, Category } from '@/lib/hotspot'
 
-function openInGoogleMaps(latitude: number, longitude: number) {
-	const url = Platform.select({
-		ios: `http://maps.apple.com/?q=${latitude},${longitude}&ll=${latitude},${longitude}`,
-		android: `geo:${latitude},${longitude}?q=${latitude},${longitude}`,
-	});
-
-	Linking.openURL(url ?? '');
-}
-
-type Props = {
-	visible: boolean;
-	id: string;
-	onClose: () => void;
+type Params = {
+  id: string;
 };
 
-export default function ModalHotspot({ visible, id, onClose }: Props) {
+const HotspotPage: React.FC = () => {
+
+	let { id } = useLocalSearchParams<Params>();
+
+	const insets = useSafeAreaInsets();
+
+	const [authToken, setAuthToken] = useState('');
 	const [context, setContext] = useState(null);
 	const [authenticated, setAuthenticated] = useState<boolean>(false);
+
 	const [hotspots, setHotspots] = useState<Hotspot[]>([]);
 	const [likes, setLikes] = useState<number>(0);
 	const [likedByMe, setLikedByMe] = useState<boolean>(false);
@@ -41,13 +38,10 @@ export default function ModalHotspot({ visible, id, onClose }: Props) {
 	const [subscribed, setSubscribed] = useState<boolean>(false);
 
 	useEffect(() => {
+
 		const init = async () => {
 			const token = await AsyncStorage.getItem('authToken');
-
-			//console.log('visible = ', visible, 'id = ', id);
-
-			if (!visible)
-				return;
+			setAuthToken(token ?? '');
 
 			const contextStr = await AsyncStorage.getItem('context');
 			const ctx = contextStr ? JSON.parse(contextStr) : {};
@@ -56,10 +50,11 @@ export default function ModalHotspot({ visible, id, onClose }: Props) {
 
 			if (token)
 				getHotspot(token, id);
-		};
+		}
 
 		init();
-	}, [visible, id]);
+
+	}, [id]);
 
 	useEffect(() => {
 
@@ -70,6 +65,37 @@ export default function ModalHotspot({ visible, id, onClose }: Props) {
 		}
 
 	}, [hotspots]);
+
+	const getHotspot = async (token: string, id: string) => {
+
+		console.log(`[HotspotPage.getHotspot] Getting hotspot ${id}`);
+
+		try {
+			const response = await fetch(`${process.env.EXPO_PUBLIC_API_BASE_URL}/hotspot/${id}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: token,
+				},
+			});
+
+			if (!response.ok) {
+				console.log('[HotspotPage.getHotspot] ', response);
+				throw new Error('Error ' + response.status + ' ' + response.statusText);
+			}
+
+			const data: Hotspot[] = await response.json();
+			setHotspots(data);
+			setLoaded(true);
+
+			//Alert.alert('', JSON.stringify(data))
+		} catch (error: any) {
+			console.log('[ModalHotspot.getHotspot] ', error);
+			Alert.alert('Error', error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
 
 	const handleLike = async (like: boolean) => {
 
@@ -138,92 +164,37 @@ export default function ModalHotspot({ visible, id, onClose }: Props) {
 		}
 	}
 
-	const getHotspot = async (token: string, id: string) => {
-
-		//console.log(`${process.env.EXPO_PUBLIC_API_BASE_URL}/hotspot/${id}`);
-
-		try {
-			const response = await fetch(`${process.env.EXPO_PUBLIC_API_BASE_URL}/hotspot/${id}`, {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: token,
-				},
-			});
-
-			if (!response.ok) {
-				console.log('[ModalHotspot.Like] ', response);
-				throw new Error('Error ' + response.status + ' ' + response.statusText);
-			}
-
-			const data: Hotspot[] = await response.json();
-			setHotspots(data);
-			setLoaded(true);
-
-			//Alert.alert('', JSON.stringify(data))
-		} catch (error: any) {
-			console.log('[ModalHotspot] ', error);
-			Alert.alert('Error', error.message);
-		} finally {
-			setLoading(false);
-		}
-	};
-
-	const stylesModal = StyleSheet.create({
-		overlay: {
-			flex: 1,
-			backgroundColor: 'rgba(0,0,0,0.5)',
-			justifyContent: 'center',
-			alignItems: 'center',
-		},
-		content: {
-			paddingHorizontal: 20,
-			paddingTop: 10,
-			paddingBottom: 20,
-			backgroundColor: 'white',
-			borderRadius: 10,
-			elevation: 5,
-			minWidth: 350
-		},
-		text: {
-			marginBottom: 10,
-			fontSize: 16,
-		},
-		map: {
-			/*width: Dimensions.get('window').width,
-			height: Dimensions.get('window').height,*/
-			width: 350,
-			height: 350,
-		},
-		closeButton: {
-			position: 'absolute',
-			top: 10,
-			right: 10,
-			zIndex: 1,
-		},
-	});
-
 	return (
-		<Modal
-			visible={visible}
-			animationType="slide"
-			transparent
-			onRequestClose={() => onClose()}
-		>
+		<View style={{
+			paddingTop: insets.top,
+			paddingBottom: /*insets.bottom*/ 0,
+			paddingLeft: insets.left,
+			paddingRight: insets.right,
+			flex: 1,
+			backgroundColor: '#f0f0f0',
+		}}>
+			<View style={styles.rowLeft}>
+				<TouchableOpacity style={{ marginHorizontal: 10, marginVertical: 10 }} onPress={() => router.canGoBack() ? router.back() : router.replace('/(tabs)')}>
+					<Ionicons name="chevron-back" size={24} color="black" />
+				</TouchableOpacity>
+				<Text style={styles.sectionTitle}>Hotspot</Text>
+			</View>
 
 			{loading && (<View><Text>Loading...</Text></View>)}
 
-
 			{loaded && (
-				<View style={stylesModal.overlay}>
-					<View style={stylesModal.content}>
-						<TouchableOpacity onPress={() => onClose()} style={stylesModal.closeButton}>
-							<Ionicons name="close" size={24} color="black" />
-						</TouchableOpacity>
+				<View style={styles.container}>
 
-						<Text style={{ fontSize: 18, fontWeight: "bold", marginVertical: 5 }}>{hotspots[0].name}</Text>
-						<Text style={{ fontSize: 10, fontStyle: "italic", marginBottom: 8, color: "gray" }}>Created by {hotspots[0].owner}</Text>
-						<Text style={{ fontSize: 12, marginBottom: 8, color: "slategray" }}>{hotspots[0].description}</Text>
+					{/* Name */}
+					<Text style={styles.label}>{hotspots[0].name}</Text>
+					<Text style={{ fontSize: 10, fontStyle: "italic", marginBottom: 8, color: "gray" }}>Created by {hotspots[0].owner}</Text>
+
+					{/* Description */}
+					<Text style={styles.label}>{hotspots[0].description}</Text>
+
+					{/* Category */}
+					{/* <Text style={styles.label}>{hotspots[0].category}</Text>*/}
+
 
 						<View style={[styles.rowLeft, { marginVertical: 8 }]}>
 
@@ -270,35 +241,11 @@ export default function ModalHotspot({ visible, id, onClose }: Props) {
 							}
 						</View>
 						
-						{/* Open details page */}
-						<TouchableOpacity
-							style={[styles.rowLeft, { marginVertical: 8 }]}
-							onPress={() => {
-								router.replace(`/hotspot-page?id=${hotspots[0].id}`);
-							}}
-						>
-							<Ionicons name="clipboard-outline" size={25} color="green" />
-							<Text>Open hotspot page</Text>
-						</TouchableOpacity>
-						
-						{/* Open in Maps */}
-						<TouchableOpacity
-							style={[styles.rowLeft, { marginVertical: 8 }]}
-							onPress={() => {
-								const { latitude, longitude } = hotspots[0].position;
-								openInGoogleMaps(latitude, longitude);
-							}}
-						>
-							<Ionicons name="location-outline" size={25} color="steelblue" />
-							<Text>Open in Maps</Text>
-						</TouchableOpacity>
-
-					</View>
 				</View>
 			)}
+		</View >
 
-		</Modal>
 	);
+};
 
-
-}
+export default HotspotPage;
